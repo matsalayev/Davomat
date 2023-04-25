1b21ad27d789db74a2fb3e307f2d2d0f5324cb6b
Azizbek Matsalayev <matsalayev@outlook.com>
2023-04-25 10:38:53 +0500

    .

--- Davomat/Form1.Designer.cs
+++ Davomat/Form1.Designer.cs
@@ -36,12 +36,17 @@
             comboBox2 = new ComboBox();
             panel2 = new Panel();
             pnlStudents = new Panel();
+            panel4 = new Panel();
+            button1 = new Button();
+            lblSon = new Label();
+            label5 = new Label();
             panel3 = new Panel();
             label4 = new Label();
             label3 = new Label();
             dateTimePicker1 = new DateTimePicker();
             panel1.SuspendLayout();
             panel2.SuspendLayout();
+            panel4.SuspendLayout();
             panel3.SuspendLayout();
             SuspendLayout();
             // 
@@ -129,10 +134,11 @@
             panel2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             panel2.AutoSize = true;
             panel2.Controls.Add(pnlStudents);
+            panel2.Controls.Add(panel4);
             panel2.Controls.Add(panel3);
             panel2.Location = new Point(31, 190);
             panel2.Name = "panel2";
-            panel2.Size = new Size(1153, 547);
+            panel2.Size = new Size(1156, 560);
             panel2.TabIndex = 3;
             // 
             // pnlStudents
@@ -141,8 +147,52 @@
             pnlStudents.Dock = DockStyle.Fill;
             pnlStudents.Location = new Point(0, 39);
             pnlStudents.Name = "pnlStudents";
-            pnlStudents.Size = new Size(1153, 508);
-            pnlStudents.TabIndex = 1;
+            pnlStudents.Size = new Size(1156, 473);
+            pnlStudents.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(button1);
+            panel4.Controls.Add(lblSon);
+            panel4.Controls.Add(label5);
+            panel4.Dock = DockStyle.Bottom;
+            panel4.Location = new Point(0, 512);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(1156, 48);
+            panel4.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top;
+            button1.Font = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(881, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(154, 41);
+            button1.TabIndex = 3;
+            button1.Text = "Saqlash";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // lblSon
+            // 
+            lblSon.Anchor = AnchorStyles.Top;
+            lblSon.AutoSize = true;
+            lblSon.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            lblSon.Location = new Point(290, 6);
+            lblSon.Name = "lblSon";
+            lblSon.Size = new Size(44, 25);
+            lblSon.TabIndex = 2;
+            lblSon.Text = "0 ta";
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.Top;
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(218, 6);
+            label5.Name = "label5";
+            label5.Size = new Size(66, 25);
+            label5.TabIndex = 1;
+            label5.Text = "Jami : ";
             // 
             // panel3
             // 
@@ -152,15 +202,15 @@
             panel3.Dock = DockStyle.Top;
             panel3.Location = new Point(0, 0);
             panel3.Name = "panel3";
-            panel3.Size = new Size(1153, 39);
+            panel3.Size = new Size(1156, 39);
             panel3.TabIndex = 0;
             // 
             // label4
             // 
-            label4.Anchor = AnchorStyles.Top;
+            label4.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
-            label4.Location = new Point(890, 6);
+            label4.Location = new Point(856, 6);
             label4.Name = "label4";
             label4.Size = new Size(89, 25);
             label4.TabIndex = 1;
@@ -168,10 +218,9 @@
             // 
             // label3
             // 
-            label3.Anchor = AnchorStyles.Top;
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
-            label3.Location = new Point(158, 6);
+            label3.Location = new Point(151, 6);
             label3.Name = "label3";
             label3.Size = new Size(261, 25);
             label3.TabIndex = 0;
@@ -205,6 +254,8 @@
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             panel2.ResumeLayout(false);
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
             panel3.ResumeLayout(false);
             panel3.PerformLayout();
             ResumeLayout(false);
@@ -223,7 +274,11 @@
         private Panel panel3;
         private Label label4;
         private Label label3;
-        private Panel pnlStudents;
         private DateTimePicker dateTimePicker1;
+        private Panel panel4;
+        private Button button1;
+        private Label lblSon;
+        private Label label5;
+        private Panel pnlStudents;
     }
 }
--- Davomat/Form1.cs
+++ Davomat/Form1.cs
@@ -1,18 +1,24 @@
-using Npgsql;
-using System.Data;
+//using Npgsql;
+//using System.Data;
+
+using Microsoft.Win32.SafeHandles;
 
 namespace Davomat
 {
     public partial class Form1 : Form
     {
-        private string connectionString = String.Format("Server = localhost; Port=5432; User Id = postgres; Password = matsalayev; Database = kidt;");
-        private NpgsqlConnection conn;
-        private string sql;
-        private NpgsqlCommand cmd;
-        private DataTable data;
+        //private string connectionString = String.Format("Server = localhost; Port=5432; User Id = postgres; Password = matsalayev; Database = kidt;");
+        //private NpgsqlConnection conn;
+        //private string sql;
+        //private NpgsqlCommand cmd;
+        //private DataTable data;
+        CheckBox[] davomat;
         public Form1()
         {
             InitializeComponent();
+            davomat = new CheckBox[25];
+            showStudents();
+
         }
         private void btnInfo_Click(object sender, EventArgs e)
         {
@@ -23,6 +29,48 @@ namespace Davomat
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        void showStudents()
+        {
+            pnlStudents.Controls.Clear();
+            int soni = 0;
+            for (int i = 0; i < 25; i++)
+            {
+                Panel panel = new Panel();
+                panel.Size = new Size(1156, 35);
+                panel.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+                panel.BorderStyle = BorderStyle.None;
+                panel.Location = new Point(0, 0 + 35 * i);
+                if (i % 2 == 0) panel.BackColor = Color.FromArgb(222, 222, 222);
+                else panel.BackColor = Color.FromArgb(240, 240, 240);
+                Label label = new Label();
+                label.Anchor = AnchorStyles.Left;
+                label.Font = new Font("Segoe UI", 10.8F);
+                label.Location = new Point(10, 6);
+                label.Size = new Size(261, 25);
+                label.Text = (i + 1) + ". Polonkasov polonkas";
+                panel.Controls.Add(label);
+                davomat[i] = new CheckBox();
+                davomat[i].Anchor = AnchorStyles.Right;
+                davomat[i].CheckedChanged += new EventHandler(checkBox_CheckedChanged);
+                davomat[i].Checked = false;
+                davomat[i].Location = new Point(915, 6);
+                panel.Controls.Add(davomat[i]);
+                pnlStudents.Controls.Add(panel);
+                soni++;
+            }
+            lblSon.Text = soni.ToString() + " ta";
+        }
+
+        private void checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+            int raqam = 0;
+            foreach (var temp in davomat)
+            {
+                if (!temp.Checked) raqam++;
+            }
+            lblSon.Text = raqam + " ta";
         }
     }
 }
